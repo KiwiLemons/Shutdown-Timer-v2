f335c214c66299f2fdd2912b8285c7ce8cd007ea
KiwiLemons <26863753+KiwiLemons@users.noreply.github.com>
2018-01-13 23:13:32 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Shutdown_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(147, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Minutes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 11);
+            this.textBox1.MaxLength = 18;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.textBox1, "Type the time that can be interpreted as either minutes or hours!\r\nTIP: You can a" +
+        "lso press enter to set a time without having the press the \"Set\" button!");
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(215, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hours";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shutdown",
+            "Reboot",
+            "Logoff"});
+            this.comboBox1.Location = new System.Drawing.Point(273, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(99, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Abort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 20000;
+            this.toolTip1.InitialDelay = 300;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 75);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shutdown Timer by KiwiLemons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Shutdown_Timer
+{
+    public partial class Form1 : Form
+    {
+        Process process = new Process();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Form load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+        //Main textbox keypress
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+                button1.PerformClick();
+        }
+        //Set button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int Time) && !textBox1.Text.StartsWith("0"))
+            {
+                //If hours is checked
+                if (radioButton2.Checked)
+                {
+                    //We need to multiply by 3600 to convert however many hours desired into seconds as the command we are trying to execute only accepts seconds.
+                    Time *= 3600;
+                }
+                //If minutes is checked
+                else
+                {
+                    Time *= 60;
+                }
+                ProcessStartInfo setStartInfo = new ProcessStartInfo();
+                setStartInfo.FileName = "cmd";
+                setStartInfo.RedirectStandardError = true;
+                setStartInfo.UseShellExecute = false;
+                setStartInfo.CreateNoWindow = true;
+                if (comboBox1.SelectedIndex == 0)
+                    //Shutdown args
+                    setStartInfo.Arguments = String.Format("/c shutdown -s -t {0}", Time);
+                else if (comboBox1.SelectedIndex == 1)
+                    //Reboot args
+                    setStartInfo.Arguments = String.Format("/c shutdown -s -r {0}", Time);
+                else
+                    //Logoff args
+                    setStartInfo.Arguments = String.Format("/c shutdown -s -l {0}", Time);
+                process.StartInfo = setStartInfo;
+                process.Start();
+                StreamReader streamreader = process.StandardError;
+                if (streamreader.ReadToEnd().Contains("1190"))
+                    if (MessageBox.Show("A timer is already in place!\nWould you like to abort it?", "Duplicate Timer", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
+                        button2.PerformClick();
+                streamreader.Close();
+            }
+            else if (textBox1.Text == "")
+                MessageBox.Show("Please do not leave the textbox empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //Have to use index because a contains would not work or just a simple == "0" because the user can have multiple zeros and it would still instantly shutdown thier PC
+            else if (textBox1.Text.IndexOf('0') == 0)
+                MessageBox.Show("Zero is a bad number to use as a timer", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                MessageBox.Show("The textbox can only contain numbers!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+        //Abort button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo abortstartinfo = new ProcessStartInfo();
+            abortstartinfo.FileName = "cmd";
+            abortstartinfo.Arguments = "/c shutdown -a";
+            //Fixes works instead of setting the window style to hidden (Having the "UseShellExecute" property set to true stops window styles from working for some reason)
+            abortstartinfo.CreateNoWindow = true;
+            abortstartinfo.RedirectStandardError = true;
+            abortstartinfo.UseShellExecute = false;
+            process.StartInfo = abortstartinfo;
+            process.Start();
+            StreamReader streamreader = process.StandardError;
+            if (streamreader.ReadToEnd().Contains("1116"))
+                MessageBox.Show("A shutdown timer was never set","No Timer",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            streamreader.Close();
+        }
+    }
+}
